29dc8d0ab99b2ae88653adbff915b59079ec1535
Josias Meneses <84454260+Josiasmeneses@users.noreply.github.com>
2023-04-01 10:07:36 -0300

    Config Project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react'
+import type { AppProps } from 'next/app';
+import GlobalStyle from '@/styles/global';
+import { ThemeProvider } from 'styled-components';
+import theme from '@/styles/theme';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const  App: React.FC <AppProps> = ({ Component, pageProps }) =>{
+  return(
+    <ThemeProvider theme={theme}>
+    <Component {...pageProps} />
+    <GlobalStyle/>
+    </ThemeProvider>
+  )
+
 }
+
+export default App
